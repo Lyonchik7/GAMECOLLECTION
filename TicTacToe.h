0b0e727be951789fe859cfc5f038a61bb0111aba
Lyonchik7 <154012140+Lyonchik7@users.noreply.github.com>
2026-05-28 22:16:02 +0300

    Update TicTacToe.h
    Изменения заголовочного файла для крестиков-ноликов

--- TicTacToe.h
+++ TicTacToe.h
@@ -1,10 +1,11 @@
 #ifndef TICTACTOE_H
 #define TICTACTOE_H
 
-#include <QWidget>
 #include <QVector>
-
-class QPushButton;
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QTimer>
 
 class TicTacToe : public QWidget
 {
@@ -13,19 +14,47 @@ class TicTacToe : public QWidget
 public:
     explicit TicTacToe(QWidget *parent = nullptr);
 
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
 private slots:
     void onButtonClick();
     void restartGame();
+    void toggleTheme();
+    void switchMode();
+    void updateTimerDisplay();
+    void timeOut();
 
 private:
     void checkGameState();
     void disableAllButtons();
     void enableAllButtons();
+    void highlightWinningButtons(const QVector<QPushButton*> &winningBtns);
+    void showWinnerMessage(const QString &winnerText);
+    void updateScoreDisplay();
+    void stopTimer();
+    void startTimerForCurrentPlayer();
 
     QVector<QVector<QPushButton*>> buttons;
+
     QString currentPlayer;
     bool gameEnded;
     int moveCount;
+    int winningRow1, winningCol1, winningRow2, winningCol2;
+
+    int scoreX;
+    int scoreO;
+    QLabel *scoreLabel;
+    QLabel *timerLabel;
+    QPushButton *themeButton;
+    QPushButton *modeButton;
+    QPushButton *restartBtn;
+
+    bool isDarkTheme;
+    bool isTurboMode;
+    int timeLimit;
+    QTimer *gameTimer;
+    int remainingTime;
 };
 
 #endif // TICTACTOE_H
